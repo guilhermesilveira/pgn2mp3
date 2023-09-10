ecb98d09c61a333a3144d6596b4947cdbb58ca7a
Guilherme Silveira <guilherme.silveira@alura.com.br>
2023-09-10 17:52:09 -0300

    simpler caching

--- cache.py
+++ cache.py
@@ -1,11 +1,6 @@
 import hashlib
 import os
 
-def create_hash(input_string):
-    hash_object = hashlib.sha512(input_string.encode())
-    hex_dig = hash_object.hexdigest()
-    return hex_dig
-
 class Cache:
     def __init__(self, cache_path:str):
         self.cache_path = cache_path
@@ -15,7 +10,6 @@ class Cache:
     def get(self, key, extension):
         # if file with key name exists, return its path
         # otherwise, return None
-        hash_key = create_hash(key)
         file_path = self.get_key_path(key, extension)
         if os.path.exists(file_path):
             return file_path
@@ -25,6 +19,4 @@ class Cache:
         os.makedirs(f"{self.cache_path}/{path}", exist_ok=True)
     
     def get_key_path(self, key, extension):
-        # hash_key = create_hash(key)
-        hash_key = key
-        return f"{self.cache_path}/{hash_key}.{extension}"
+        return f"{self.cache_path}/{key}.{extension}"
