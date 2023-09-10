0717276d6746b840bb507fbfb768c13a00e8d7a0
Guilherme Silveira <guilherme.silveira@alura.com.br>
2023-09-10 17:52:25 -0300

    less verbose

--- tts.py
+++ tts.py
@@ -30,13 +30,14 @@ class GoogleVoicer:
         name = name.replace("/", "_")
         name = name.replace(":", "_")
         name = name.replace(" ", "_")
+        name = name.replace("'", "_")
         name = name.replace("?", "_")
         cached = self.slow_cache.get(name, "mp3")
         if cached:
             return cached
 
         caption = f"<speak>{caption}</speak>"
-        tqdm.write(f"Speaking: {caption}")
+        # tqdm.write(f"Speaking: {caption}")
         synthesis_input = texttospeech.SynthesisInput(ssml=caption)
         response = self.client.synthesize_speech(
             input=synthesis_input,
@@ -47,10 +48,10 @@ class GoogleVoicer:
             out.write(response.audio_content)
 
         # ffmpeg file to slow down to 74% with same file name but folder same_folder/74/
-        # ffmpeg -i file -filter:a "atempo=0.74" -vn new_file
-        # create the string
         new_name = self.slow_cache.get_key_path(name, 'mp3')
-        ffmpeg_string = f"ffmpeg -y -i '{file}' -filter:a \"atempo=0.74\" -vn '{new_name}'"
+        silence_it = " -hide_banner -loglevel panic "
+        ffmpeg_string = f"ffmpeg -y {silence_it} -i '{file}' -filter:a \"atempo=0.74\" -vn '{new_name}'"
+
         # runs it, if fails, stop
         if os.system(ffmpeg_string):
             logging.error(f"Failed to run ffmpeg command: {ffmpeg_string}")
